31e26d65e5a5ff679c0b8e6d1fcd57e322a4122c
Gary Johnson <jazzbassjohnson@gmail.com>
2014-09-12 12:44:12 -0700

    test(scope_spec) scope does not end the digest so that new watches are not run

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -166,4 +166,25 @@ describe("digest", function() {
     scope.$digest();
     expect(watchExecutions).toBe(301);
   });
+
+  it("does not end the digest so that new watches are not run", function() {
+    scope.aValue = 'abc';
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.$watch(
+          function(scope) { return scope.aValue; },
+          function(newValue, oldValue, scope) {
+            scope.counter++;
+          }
+        );
+      }
+    );
+
+    scope.digest();
+    expect(scope.counter).toBe(1);
+
+  });
 });
