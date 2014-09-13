2deefdb90be46c0b3efa1518ace935096df7424a
Gary Johnson <jazzbassjohnson@gmail.com>
2014-09-13 12:48:38 -0700

    test(scope_spec) 22530postDigest functions are not run durring a digest

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -398,4 +398,28 @@ describe("digest", function() {
     expect(scope.counter).toBe(1);
 
   })
+
+  it("does not include $$postDigest in the digest", function() {
+    scope.aValue = 'original value';
+
+    scope.$$postDigest(function() {
+      scope.aValue = 'changed value';
+    });
+
+    scope.$watch(
+      function(scope) { return scope.aValue; },
+      function(newValue, oldValue, scope) {
+        scope.watchedValue = newValue;
+      }
+    );
+    
+    scope.$digest();
+    expect(scope.watchedValue).toBe('original value');
+
+    scope.$digest();
+    expect(scope.watchedValue).toBe('changed value');
+
+  });
+
+
 });
