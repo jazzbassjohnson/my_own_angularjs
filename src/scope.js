3359b5226b86f9f016067a7aeaef53966f664f0f
Gary Johnson <jazzbassjohnson@gmail.com>
2014-09-15 22:07:17 -0700

    refactor(scope.js) digestOnce digests children's scopes

--- src/scope.js
+++ src/scope.js
@@ -67,32 +67,36 @@ Scope.prototype.$digest = function() {
 };
 
 Scope.prototype.$$digestOnce = function() {
+    var dirty;
+    var continueLoop = true;
     var self = this;
-    var newValue, oldValue, dirty;
+    this.$$everyScope(function(scope) {
+        var newValue, oldValue;
+        _.forEachRight(scope.$$watchers, function(watcher) {
+            try {
+                if(watcher) {
+                    newValue = watcher.watchFn(scope);
+                    oldValue = watcher.last;
 
-    _.forEachRight(this.$$watchers, function(watcher) {
-        try {
-            if(watcher) {
-                newValue = watcher.watchFn(self);
-                oldValue = watcher.last;
-
-                if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-                    self.$$lastDirtyWatch = watcher;
-                    watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-                    watcher.listenerFn(newValue, 
-                        (oldValue === initWatchVal ? newValue : oldValue),
-                        self);
-                  dirty = true;
-                } else if(self.$$lastDirtyWatch === watcher) {
-                    return false;
+                    if (!scope.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                        self.$$lastDirtyWatch = watcher;
+                        watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                        watcher.listenerFn(newValue, 
+                            (oldValue === initWatchVal ? newValue : oldValue),
+                            scope);
+                      dirty = true;
+                    } else if(self.$$lastDirtyWatch === watcher) {
+                        continueLoop = false;
+                        return false;
+                    }
+                    
                 }
-                
+            } catch (e) {
+                console.error(e);
             }
-            
-        } catch (e) {
-            console.error(e);
-        }
-    }); 
+        }); 
+        return continueLoop;
+    });
     return dirty;
 };
 
