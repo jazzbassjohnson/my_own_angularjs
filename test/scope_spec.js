b9a981077dd253d93d23c3d3411e6cd3f11f34b5
Gary Johnson <jazzbassjohnson@gmail.com>
2014-09-30 16:10:02 -0700

    test(scope_spec) watchCollection function works like a normal watch for non-collections

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -12,7 +12,7 @@ describe("Scope", function() {
     expect(scope.aProperty).toBe(1);
   });
   
-  describe("digest", function() {
+  describe("$digest", function() {
     var scope;
 
     beforeEach(function() {
@@ -940,6 +940,46 @@ describe("Scope", function() {
       expect(child.counter).toBe(2);
 
     });
+  });
+
+  describe("$watchCollection", function() {
+    var scope;
+
+    beforeEach(function() {
+      jasmine.clock().install();
+      scope = new Scope();
+    });
+
+    afterEach(function() {
+      jasmine.clock().uninstall();
+    });
+
+    it("works like a normal watch for non-collections", function(){
+      var valueProvided;
+
+      scope.aValue = 42;
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.aValue; },
+        function(newValue, oldValue, scope) {
+          valueProvided = newValue;
+          scope.counter++;
+        }
+      );
+
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      expect(valueProvided).toBe(scope.aValue);
+
+      scope.aValue = 43;
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+    });
+
 
 
   });
