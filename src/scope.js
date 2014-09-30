9e1d1b9a3a3a14a0e2025ead50d830925a13c7a9
Gary Johnson <jazzbassjohnson@gmail.com>
2014-09-30 15:30:42 -0700

    feat(scope.js) child scope is no longer digested when  has been called

--- src/scope.js
+++ src/scope.js
@@ -179,5 +179,17 @@ Scope.prototype.$new = function(isolated) {
     child.$$postDigestQueue = this.$$postDigestQueue;
     child.$$watchers = [];
     child.$$children = [];
+    child.$parent = this;
     return child;
+};
+
+Scope.prototype.$destroy = function(){
+  if(this === this.$$root) {
+    return;
+  }
+  var siblings = this.$parent.$$children;
+  var indexOfThis = siblings.indexOf(this);
+  if(indexOfThis >= 0) {
+    siblings.splice(indexOfThis, 1);
+  }
 };
