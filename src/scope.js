1fcdfaa912affc9aa0a95185ef2b8fe392148118
Gary Johnson <jazzbassjohnson@gmail.com>
2014-10-15 16:37:52 -0700

    refactor(scope.js) use isArrayLike function in watchCollection function

--- src/scope.js
+++ src/scope.js
@@ -205,7 +205,7 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
         newValue = watchFn(scope);
 
         if(_.isObject(newValue)) {
-            if(_.isArray(newValue)) {
+            if(_.isArrayLike(newValue)) {
                 if(!_.isArray(oldValue)) {
                     changeCounter++;
                     oldValue = [];
